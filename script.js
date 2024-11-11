5a0c3bef9b4b409d5391bd9a377d2443aa0a5714
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-11-11 11:21:36 +0300

    added answer 3

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ Exercise 1: Array.prototype.filter()
 */
 
 // Complete the exercise in the space below:
-let veryOldInventors = inventors.filter((oldInventor) => {
+const veryOldInventors = inventors.filter((oldInventor) => {
   return oldInventor.year >= 1500 && oldInventor.year <= 1599
 })
 // Check your work:
@@ -124,4 +124,28 @@ console.log('Exercise 2 correct result: ', [
   { first: 'Sarah E.', last: 'Goode' },
   { first: 'Lise', last: 'Meitner' },
   { first: 'Hanna', last: 'Hammarström' }
+])
+/*
+Exercise 3: Array.prototype.sort()
+*/
+// Complete the exercise in the space below:
+const sortedByBirthYear = inventors.sort((year1, year2) => {
+  return year1.year - year2.year
+})
+
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear)
+console.log('Exercise 3 correct result: ', [
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
 ])
